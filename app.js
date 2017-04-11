324cad05d98c9ae68e501b028a75e8e8d4367401
David Zuniga <David Zuniga>
2017-04-11 01:10:42 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('I am ready');
+});
+
+bot.on('message', message => {
+    if(message.content === 'ping') {
+        message.reply('pong');
+        message.author.sendMessage("holis");
+    }
+});
+
+bot.on("guildMemberAdd", (member) => {
+    member.sendMessage("Esto es una prueba y no hay pack");
+});
+
+
+bot.login('Mjk5NjkyNTk4NjUyODI5Njk2.C8sqQA.9jB2khnLNnKvSs9qNb74IlbNsy0');
