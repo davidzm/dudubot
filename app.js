ae0f55d47d26883f47e5dbb4ec4caa632a7ed4a9
David Zuniga <dazoram@gmail.com>
2017-11-30 19:25:02 -0500

    debug

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ bot.on('message', message => {
         message.author.sendMessage("holis");
     }
     if(message.content === 'feliz' || message.content === 'amor') {
+    	message.reply(message.author);
+    	message.reply(message.author.nick);
     	if(message.author.nick === 'Chiprel') {
     		message.reply('Mira tus mensajes privados ;)');
         	message.author.sendMessage("Ahora visita acá: https://month3.herokuapp.com/");
